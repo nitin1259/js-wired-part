c37a2417c4fc6920531534e374c19db359c349e0
Nitin SINGH <nitinkumar.singh@f5.com>
2021-04-20 11:25:28 +0530

    reflection and extend exaple from underscore js

--- app1.js
+++ app1.js
@@ -572,7 +572,7 @@ console.log(arr5);
 // simple defination of inheritance is - "one object get access to the property and method of another object"
 
 // prototype chain
-
+/*
 var person = {
   fname: "default",
   lname: "default",
@@ -601,3 +601,46 @@ jane = {
 jane.__proto__ = person;
 console.log(jane.getFullName());
 console.log(jane.lname);
+
+*/
+
+// Reflection - an object can look at itself, listening and changing its properties and methods
+
+var person = {
+  firstname: "Default",
+  lastname: "Default",
+  getFullName: function () {
+    return this.firstname + " " + this.lastname;
+  },
+};
+
+var john = {
+  firstname: "John",
+  lastname: "Doe",
+};
+
+// don't do this EVER! for demo purposes only!!!
+john.__proto__ = person;
+
+for (var prop in john) {
+  if (john.hasOwnProperty(prop)) {
+    console.log(prop + ": " + john[prop]);
+  }
+}
+
+var jane = {
+  address: "111 Main St.",
+  getFormalFullName: function () {
+    return this.lastname + ", " + this.firstname;
+  },
+};
+
+var jim = {
+  getFirstName: function () {
+    return firstname;
+  },
+};
+
+_.extend(john, jane, jim);
+
+console.log(john);
